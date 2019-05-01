b3e2e2974b27044c8b379f83bfe458fef1f4cdbf
andywu0489 <andywu0489@gmail.com>
2019-05-01 11:47:00 +0800

    Edit box shadow around inputs

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -3,6 +3,7 @@ const onCancel = () => {
   $('form').trigger('reset')
   $('.inputs').css('border', '2px solid rgb(224, 224, 224)')
   $('.error-msg').text('')
+  $('.inputs').css('box-shadow', 'none')
   console.log('cancel')
 }
 
@@ -12,20 +13,12 @@ const validateTitle = (title) => {
     $(() => {
       $('#position-title-msg').text('Position Title is required.')
       $('#title').css('border', '2px solid red')
-      // $('#title').css('outline', '2px solid red')
-      // $('#title').css('border-color', 'red')
-      // $('#title').css('box-shadow', '0 0 5px red')
+      $('#title').css('box-shadow', '0 0 5px red')
     })
   } else {
     $(() => {
       $('#title').css('border', '2px solid rgb(224, 224, 224)')
-      // $('#title').css('outline', '1px solid rgb(51, 153, 255)')
-      // $('#title:focus').css('outline', 'none')
-      // $('#title:focus').css('border-color', 'rgb(51, 153, 255)')
-      // $('#title').css('border-color', 'rgb(51, 153, 255)')
-      // $('#title').css('box-shadow', 'none')
-      // $('#title').focus($('#title').css('box-shadow', '0 0 5px blue'))
-      // $('#title').css('outline', 'none')
+      $('#title').css('box-shadow', 'none')
     })
   }
 }
@@ -36,11 +29,12 @@ const validateLocation = (location) => {
     $(() => {
       $('#position-location-msg').text('Position Location is required.')
       $('#location').css('border', '2px solid red')
-      // $('#location').css('box-shadow', '0 0 5px red')
+      $('#location').css('box-shadow', '0 0 5px red')
     })
   } else {
     $(() => {
       $('#location').css('border', '2px solid rgb(224, 224, 224)')
+      $('#location').css('box-shadow', 'none')
     })
   }
 }
@@ -50,9 +44,11 @@ const validateDepartment = (department) => {
   if (department === '') {
     $('#department-msg').text('Department Title is required.')
     $('#department').css('border', '2px solid red')
+    $('#department').css('box-shadow', '0 0 5px red')
   } else {
     $(() => {
       $('#department').css('border', '2px solid rgb(224, 224, 224)')
+      $('#department').css('box-shadow', 'none')
     })
   }
 }
@@ -62,7 +58,11 @@ const validateComment = (comment) => {
   if (comment === '') {
     $('#comment-msg').text('Comment is required.')
     $('#comment').css('border', '2px solid red')
-  } else ($('#comment').css('border', '2px solid rgb(224, 224, 224)'))
+    $('#comment').css('box-shadow', '0 0 5px red')
+  } else {
+    $('#comment').css('border', '2px solid rgb(224, 224, 224)')
+    $('#comment').css('box-shadow', 'none')
+  }
 }
 
 const clearForm = (title, location, department, comment) => {
